3db3f43ceee3fcde43818f6411c6d66419d4e716
Pedro Camargo <pedro.k.s.c.sud@gmail.com>
2026-01-10 14:26:07 -0700

    worked on JS exercises

---
+++ ponder/variables-constants.js
@@ -0,0 +1,31 @@
+const PI = 3.14;
+let radius = 3; //'let'changeable variable
+
+let area = radius * radius * PI;
+
+console.log(area);
+
+radius = 20;
+area = radius * radius * PI;
+
+console.log(area);
+
+// type coersion
+const one = 1;
+const two = '2';
+
+let result = one * two
+console.log(result);
+
+result = one + Number(two);
+console.log(result);
+
+//Scope
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);
+    console.log(student);
+}
+console.log(course);
+console.log(student);
